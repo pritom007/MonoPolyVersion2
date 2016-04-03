d5124dbb6a4216ace005e59887c34d5f6cef0dae
普里通(14302016002) <naitiq@yahoo.com>
2016-04-04 00:00:28 +0800

    This is the first commit

---
+++ MonoPolyReturns/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 *this is the main function
+		 *game will start from here with the user input  
+		 */
+		
+
+	}
+
+}
