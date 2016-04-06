522c415de69a5cc45dda1d17e5ba0d4b4443fdc6
普里通(14302016002) <naitiq@yahoo.com>
2016-04-06 22:37:11 +0800

    ~GameControl.jave created 1st commit

---
+++ MonoPolyReturns/src/GameControl.java
@@ -0,0 +1,15 @@
+
+public class GameControl {
+	/*
+	 * this class mainly is to control the options 
+	 * of whole game.
+	 * here all the necessary classes will be set
+	 * as static so that we can keep track of the changes of
+	 * any property 
+	 */
+	
+	public static GameMapping gameMapping = new GameMapping();
+	public static GameFileReader gameFileReader = new GameFileReader();
+	public static Player player = new Player();
+
+}
