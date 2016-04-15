78bdfb8d52511031cef4111592c5cd1c6f0e2320
普里通(14302016002) <naitiq@yahoo.com>
2016-04-15 19:13:42 +0800

    ~some basic changes has introduced

--- MonoPolyReturns/src/Player.java
+++ MonoPolyReturns/src/Player.java
@@ -1,15 +1,21 @@
 import java.util.Iterator;
 
 
-public class Player {
+public class Player extends GameMapping{
 	
 	//the name of player
 	String playerName="player";
+	//Symbol of player
+	String symbol="";
+	
 	//numbers of player
 	int numberOfPlayer;
+	
 	//position of player if it is clockwise or anti clock wise 
 	String[] position = new String[]{"Clockwise ","Anti Clockwise"};
-	int[] positionOfPlayer = new int[]{0,0};//Said the game player direction
+	int XpositionOfPlayer = 0;//Said the game player direction
+	int YpositionOfPlayer = 0;//Said the game player direction
+	int currentPosition=0;
 	/*
 	int[] xCoOrdinate;
 	int[] yCoOrdinate;
@@ -24,19 +30,37 @@ public class Player {
 	}
 	
 	//give name and set cash in the beginning of the game
-	public Player(String playerName,int cashOfPlayer){
+	public Player(String playerName,int cashOfPlayer,String symbol){
 		this.playerName=playerName;
 		this.cashOfPlayer=cashOfPlayer;
+		this.symbol=symbol;
 	}
 	
 	//all property information of the player
-	public void allPlayerInformation(){
+	public void allPlayerInformation(Player[] players){
 		System.out.println("player name\tpoint\tcash\tDeposite\tHouse property\tThe total amount of real estate assets");
-		for (int i=0;i<GameControl.player.numberOfPlayer;i++) {
-			System.out.println(playerName + "\t\t" + pointOfPlayer + "\t" + cashOfPlayer + "\t" + depositOfPlayer + "\t\t" + housePropertyOfPlayer+ "\t\t" + totalMoneyOfPlayer);
+		for (int i=0;i<players.length;i++) {
+			System.out.println(players[i].playerName + "\t\t" + players[i].pointOfPlayer + "\t" + players[i].cashOfPlayer + "\t" + players[i].depositOfPlayer + "\t\t" + players[i].housePropertyOfPlayer+ "\t\t" + players[i].totalMoneyOfPlayer);
+			//System.out.println(b);
 		}
 	
 	
+	}
+
+	public int getXpositionOfPlayer() {
+		return XpositionOfPlayer;
+	}
+
+	public void setXpositionOfPlayer(int xpositionOfPlayer) {
+		XpositionOfPlayer = xpositionOfPlayer;
+	}
+
+	public int getYpositionOfPlayer() {
+		return YpositionOfPlayer;
+	}
+
+	public void setYpositionOfPlayer(int ypositionOfPlayer) {
+		YpositionOfPlayer = ypositionOfPlayer;
 	}
 	
 
