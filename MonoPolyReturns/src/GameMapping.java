e867e89b03a6fdac36f9ea8e9ddeb2c2dae8d5a6
普里通(14302016002) <naitiq@yahoo.com>
2016-04-15 19:10:52 +0800

    ~GameMapping is up to date made formatter to get the position made default map

--- MonoPolyReturns/src/GameMapping.java
+++ MonoPolyReturns/src/GameMapping.java
@@ -5,17 +5,17 @@ import java.util.Map;
 public class GameMapping  {
 	
 	//map[Xpos][Ypos]
-	private static int Xpos,Ypos;
+	private  int Xpos,Ypos;
 	//MaxX,MaxY by default we set the size 20x20 (01...19 = 20)
-	private static int MaxX = 19,MaxY = 19;
+	private int MaxX = 19,MaxY = 19;
 	
-	private static Map<String, String> mapOfGame = new HashMap<String, String>();
-	private static Map<Integer, String> posNumOfMap = new HashMap<Integer, String>(); 
+	protected Map<String, String> mapOfGame = new HashMap<String, String>();
+	protected Map<Integer, String> posNumOfMap = new HashMap<Integer, String>(); 
 	
 	//keeps track of the total number of places
-	private static int count = 0;
+	private int count = 0;
 	//map of the game
-	protected static String[][] map = new String[20][20];
+	protected String[][] map = new String[10][20];
 	
 	//constructor
 	public GameMapping() {
@@ -27,9 +27,9 @@ public class GameMapping  {
 		//checks if suggested path is valid "1" is hard coded for leftToRight()
 		if(checkMapCreator(from, to, 1)){
 			for(;from<to;Ypos++,from++,count++){
-					map[Xpos][Ypos] = "*";
-					mapOfGame.put( Xpos+""+Ypos, map[Xpos][Ypos].toString());
-					posNumOfMap.put(count, Xpos+""+Ypos);
+					map[Xpos][Ypos] = "¤";
+					mapOfGame.put( Xpos+"*"+Ypos, map[Xpos][Ypos].toString());
+					posNumOfMap.put(count, Xpos+"*"+Ypos);
 			}
 		}
 		else{
@@ -42,9 +42,9 @@ public class GameMapping  {
 	public void rightToLeft(int from,int to){
 		if(checkMapCreator(from,to,2)){
 			for(;from>to;Ypos--,from--,count++){
-				map[Xpos][Ypos] = "*";
-				mapOfGame.put(Xpos+""+Ypos, map[Xpos][Ypos].toString());
-				posNumOfMap.put(count, Xpos+""+Ypos);
+				map[Xpos][Ypos] = "¤";
+				mapOfGame.put(Xpos+"*"+Ypos, map[Xpos][Ypos].toString());
+				posNumOfMap.put(count, Xpos+"*"+Ypos);
 			}
 		}
 		else{
@@ -57,9 +57,9 @@ public class GameMapping  {
 	public void topToBottom(int from,int to){
 		if(checkMapCreator(from,to,3)){
 			for(;from<to;Xpos++,from++,count++){
-				map[Xpos][Ypos] = "*";
-				mapOfGame.put(Xpos+""+Ypos, map[Xpos][Ypos].toString());
-				posNumOfMap.put(count, Xpos+""+Ypos);
+				map[Xpos][Ypos] = "¤";
+				mapOfGame.put(Xpos+"*"+Ypos, map[Xpos][Ypos].toString());
+				posNumOfMap.put(count, Xpos+"*"+Ypos);
 			}
 		}
 		else{
@@ -71,9 +71,9 @@ public class GameMapping  {
 	public void bottomToTop(int from,int to){
 		if(checkMapCreator(from,to,4)){
 			for(;from>to;Xpos--,from--,count++){
-				map[Xpos][Ypos] = "*";
-				mapOfGame.put(Xpos+""+Ypos, map[Xpos][Ypos].toString());
-				posNumOfMap.put(count, Xpos+""+Ypos);
+				map[Xpos][Ypos] = "¤";
+				mapOfGame.put(Xpos+"*"+Ypos, map[Xpos][Ypos].toString());
+				posNumOfMap.put(count, Xpos+"*"+Ypos);
 			}
 		}
 		else{
@@ -110,5 +110,90 @@ public class GameMapping  {
 		mapOfGame.put(row+""+col, map[row][col]);
 		posNumOfMap.put(count, row+""+col);
 	}
+	/*
+	 * making a default map for the game
+	 * this can be edited
+	 */
+	public void defaultMap(){
+		
+		leftToRight(0, 8);
+		setOtherOption(0, 4, "C");
+		topToBottom(0, 1);
+		leftToRight(8, 10);
+		setOtherOption(1, 10, "N");
+		topToBottom(1, 4);
+		leftToRight(10, 14);
+		setOtherOption(4, 13, "€");
+		bottomToTop(4, 0);
+		leftToRight(14, 19);
+		setOtherOption(0, 15,"L");
+		setOtherOption(0, 16,"P");
+		setOtherOption(0, 17,"P");
+		setOtherOption(0, 18,"P");
+		setOtherOption(0, 19,"P");
+		topToBottom(0, 6);
+		setOtherOption(1, 19,"P");
+		setOtherOption(2, 19,"P");
+		setOtherOption(3, 19,"B");
+		setOtherOption(4, 19,"P");
+		setOtherOption(5, 19,"P");
+		rightToLeft(19, 16);
+		setOtherOption(6, 19,"P");
+		setOtherOption(6, 18, "P");
+		setOtherOption(6, 17, "B");
+		topToBottom(6, 8);
+		rightToLeft(16, 14);
+		bottomToTop(8, 6);
+		setOtherOption(7, 14, "S");
+		rightToLeft(14, 12);
+		topToBottom(6, 9);
+		rightToLeft(12, 9);
+		setOtherOption(9, 13, "C");
+		bottomToTop(9, 6);
+		rightToLeft(9, 6);
+		setOtherOption(6, 10, "N");
+		bottomToTop(6, 5);
+		rightToLeft(6, 2);
+		setOtherOption(5, 4, "€");
+		bottomToTop(5, 2);
+		rightToLeft(2, 0);
+		bottomToTop(2, 1);
+		setOtherOption(2, 1, "L");
+		setOtherOption(2, 0, "P");
+		setOtherOption(1, 0, "P");
+		
+	}
+	
+	/*
+	 * this function helps to get the positon of
+	 * the Player
+	 */
+	public int formatPosition(String position,char xy) {
+		System.out.println(position);
+		String partStrings[]=position.split("\\*");
+		System.out.println(partStrings[0]+" "+partStrings[1]);
+		return (xy=='X')?Integer.parseInt(partStrings[0]):Integer.parseInt(partStrings[1]);
+	}
+	
+	/*
+	 * this is to print the current map
+	 */
+	public void printMap(GameMapping mapping,Player player){
+		for (int i = 0; i < mapping.map.length; i++) {
+			for (int j = 0; j < mapping.map[i].length; j++) {
+				
+				if (i==player.XpositionOfPlayer && j==player.YpositionOfPlayer) {
+					System.out.print(player.symbol);
+				}
+				else if(mapping.map[i][j]!=null)
+					System.out.print(mapping.map[i][j]);
+				else {
+					System.out.print(" ");
+				}
+			}//end for loop
+			System.out.println();
+		}//end for loop
+	}//end method
+	
 	
 }
