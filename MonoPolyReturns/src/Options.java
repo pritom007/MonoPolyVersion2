a59e736c5ffb87891fe61b70f0defa3c4b3cd913
普里通(14302016002) <naitiq@yahoo.com>
2016-04-22 21:39:23 +0800

    ~created Options class just to make sure codes are clean. need to edit 4th option

---
+++ MonoPolyReturns/src/Options.java
@@ -0,0 +1,16 @@
+
+public class Options {
+
+	public void printOption(){
+		
+	System.out.println("Here is you options:");
+	System.out.println("0 - view current map");
+	System.out.println("1 - view the original map");
+	System.out.println("2 - watch if there is any barricade");
+	System.out.println("3 - use the cards");
+	System.out.println("4 - ");
+	System.out.println("5 - see the information of players");
+	System.out.println("6 - Roll dice and take action and change turn!");
+	System.out.println("7 - quit!");
+	}
+}
